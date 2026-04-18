893f18e5222972b00ce6a8deded503f38b5917e6
Michael Beck <mambax7@users.noreply.github.com>
2026-04-18 15:05:00 -0400

    TinyMCE default config was not actually loading the full plugin set. (#24)
    * TinyMCE default config was not actually loading the full plugin set.
    
      - uses a comma-separated plugins list, which is what the XOOPS TinyMCE 7 loader expects
      - restores xoopsimagemanager to the loaded plugins and toolbar
      - enables the menubar again
      - replaces legacy styleselect with TinyMCE 7’s styles control
      - removes insertfile, which is not needed here
    
    * removed imagetools

--- htdocs/class/xoopseditor/tinymce7/settings.php
+++ htdocs/class/xoopseditor/tinymce7/settings.php
@@ -23,15 +23,15 @@
 return [
 	'theme' => 'silver',
 	'selector' => 'textarea#summary, textarea#body, textarea#message', // keep if targeting multiple fields
-	'plugins' => 'advlist anchor autolink charmap code hr image imagetools lists link media preview searchreplace table xoopsemoticons xoopscode', // removed 'xoopsimagemanager'
-	'toolbar' => 'undo redo | styleselect | bold italic | forecolor backcolor removeformat | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | table tabledelete | insertfile image media link | hr | xoopsemoticons xoopscode | code',
+	'plugins' => 'advlist,anchor,autolink,charmap,code,hr,image,lists,link,media,preview,searchreplace,table,xoopsimagemanager,xoopsemoticons,xoopscode',
+	'toolbar' => 'undo redo | styles | bold italic | forecolor backcolor removeformat | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | table tabledelete | image media link | hr | xoopsimagemanager xoopsemoticons xoopscode | code',
 	'image_title' => true,
 	'table_use_colgroups' => true,
     // This option prevents tinyeditor from deleting empty tags
 	'valid_elements' => '*[*]',
 	'relative_urls' => false,
 	'body_class' => 'tinymce7-body',
-	'menubar' => false,
+	'menubar' => true,
 //	'content_css' => 'https://diversitybridge.net/themes/xswatch4/style.css', // newly added
 	'content_css' => XOOPS_THEME_URL . '/' . $GLOBALS['xoopsConfig']['theme_set'] . '/style.css',
 	'width' => '100%', // newly added
