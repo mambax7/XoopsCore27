83c247debe8af43f8ebbabf559ca0141aac69759
Michael Beck <mambax7@gmail.com>
2026-09-12 16:18:06 -0400

    test(2fa): add the source header to the harness scripts and the remaining new tests

--- tests/unit/htdocs/include/TwoFactorManagementTest.php
+++ tests/unit/htdocs/include/TwoFactorManagementTest.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Tests for the two-factor management helpers in include/twofactor.php
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright (c) 2000-2026 XOOPS Project (https://xoops.org)
+ * @license   GNU GPL 2 (https://www.gnu.org/licenses/gpl-2.0.html)
+ * @package   core
+ * @since     2.7.4
+ */
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
